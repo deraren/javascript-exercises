851da4576dd57eb9f3008ce67fb2cf9b966af72a
Darren Ng <darrrenngvs@gmail.com>
2024-07-11 20:27:28 +0800

    done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0)
+        {
+            return "ERROR";
+        }
+    let returnstring = "";
+    for (let i = 0; i < num; i++)
+        {
+            returnstring = returnstring.concat(string);
+        }
+    return returnstring;
 };
 
 // Do not edit below this line
